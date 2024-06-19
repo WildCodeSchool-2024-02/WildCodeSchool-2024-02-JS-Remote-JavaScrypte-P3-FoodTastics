27653af14968d0c583364851febbf8cd38d0171e
Yann Duhamel <yann.duhamel@gmail.com>
2024-06-19 17:09:23 +0200

    feat: create recipe json file and its seeder

---
+++ server/database/fixtures/RecipeSeeder.js
@@ -0,0 +1,21 @@
+const recipes = require("../data/recipes.json");
+
+const AbstractSeeder = require("./AbstractSeeder");
+
+class RecipeSeeder extends AbstractSeeder {
+  constructor() {
+    super({ table: "recipe", truncate: true });
+  }
+
+  run() {
+    for (let i = 0; i < recipes.length; i += 1) {
+      const recipe = recipes[i];
+      this.insert({
+        ...recipe,
+        refName: `recipe_${i}`,
+      });
+    }
+  }
+}
+
+module.exports = RecipeSeeder;
