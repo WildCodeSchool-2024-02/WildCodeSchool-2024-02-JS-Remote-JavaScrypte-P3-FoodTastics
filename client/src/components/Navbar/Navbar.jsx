f7d4e2307af6631d10fc61f38f70b33e2b3a8a7e
ANNE_DAMIEN <PRO.DAMIENANNE@gmail.com>
2024-06-27 12:19:43 +0200

    feat: create button connexion and place it in Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Menu from "../Menu/Menu";
+import ButtonConnection from "../ButtonConnection/ButtonConnection";
 import "./Navbar.css";
 
 export default function Navbar() {
@@ -11,14 +12,17 @@ export default function Navbar() {
 
   return (
     <div className="Navbar">
-      <button className="Logo" type="button" onClick={redirectionToHome}>
-        <img
-          className="logoImage"
-          src="/images/Logo_foodtastics.png"
-          alt="logo del sitio"
-        />
-      </button>
-      <Menu />
+      <div className="menu-logo">
+        <button className="Logo" type="button" onClick={redirectionToHome}>
+          <img
+            className="logoImage"
+            src="src/assets/images/Logo_foodtastics.png"
+            alt="logo del sitio"
+          />
+        </button>
+        <Menu />
+      </div>
+      <ButtonConnection />
     </div>
   );
 }
