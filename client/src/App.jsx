1e61259c822c35dae721ea9710f2ddb551ac2cb7
Irene Sánchez <enerisan@gmail.com>
2024-06-30 18:53:33 +0200

    feat: creating ingredient page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 import { Outlet } from "react-router-dom";
+import { ToastContainer} from "react-toastify";
+
+
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition:Bounce
+      />
       <Navbar />
       <Outlet />
       <Footer />
